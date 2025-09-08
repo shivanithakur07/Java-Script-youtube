2001e1a3d8ad232ad65cdf1cfe8d66845bebf17b
shivanithakur07 <23BAI70053@cuchd.in>
2025-09-08 13:38:10 +0000

    commit chnge

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const Account_ID=144553
+let AccountEmail = "thakueshivani6@gmail.com"
+var AccountPassword = "12345"
+accountCity = "Jaipur"
+
+// accountId = 2//not Allowed
+
+AccountEmail = "ST@gmil.com"
+AccountPassword = "67890"
+accountCity = "Delhi"
+console.log(Account_ID);
+console.table([Account_ID,AccountEmail,AccountPassword,accountCity])
+// 
