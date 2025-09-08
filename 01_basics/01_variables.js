7b9b75a211e2886e7314afd680a0e2ac485394b1
shivanithakur07 <23BAI70053@cuchd.in>
2025-09-08 13:38:27 +0000

    commit chnge

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -2,12 +2,12 @@ const Account_ID=144553
 let AccountEmail = "thakueshivani6@gmail.com"
 var AccountPassword = "12345"
 accountCity = "Jaipur"
-
+let accountState;
 // accountId = 2//not Allowed
 
 AccountEmail = "ST@gmil.com"
 AccountPassword = "67890"
 accountCity = "Delhi"
 console.log(Account_ID);
-console.table([Account_ID,AccountEmail,AccountPassword,accountCity])
+console.table([Account_ID,AccountEmail,AccountPassword,accountCity,accountState])
 // 
