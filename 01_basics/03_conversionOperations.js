315dc67c9bc62d4cfbea9e7c63e948e2f0c648bf
shivanithakur07 <23BAI70053@cuchd.in>
2025-09-10 13:03:41 +0000

    converion of type

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,37 @@
+let score = "33"
+//console.log(typeof score);
+//console.log(typeof (score));
+
+
+
+let valueinNumber = Number(score)
+//console.log(typeof valueinNumber);
+//console.log(valueinNumber);
+
+
+
+//"33"=>33
+//"33abc"=>NaN
+//true=>1; false=>0
+
+let isloggedin = "Shivani"
+
+let booleanisloggedin = Boolean(isloggedin)
+// console.log(booleanisloggedin);
+
+
+
+//1=>true
+//0=>false
+//""=>false
+//"shivani"=>true
+
+
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
+
+
+
+
