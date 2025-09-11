5edd197165f468c86a825f4e027d2f76cc0355d5
shivanithakur07 <23BAI70053@cuchd.in>
2025-09-11 13:00:48 +0000

    commit chnge

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -29,9 +29,58 @@ let booleanisloggedin = Boolean(isloggedin)
 
 
 let someNumber = 33
-let stringNumber = String(someNumber)
-console.log(typeof stringNumber);
+// let stringNumber = String(someNumber)
+// console.log(typeof stringNumber);
 
 
+
+// ***************************************Opertions******************************************
+
+let value = 3
+let negvalue = -value
+// console.log(negvalue);
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(5%2);
+// console.log(5**2); //5^2
+// console.log(5**3); //5^3
+
+
+let str1 = "Hello"
+let str2 = " World"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1"+2); //12
+
+
+// console.log(1+"2"); //12
+
+// console.log("1"+2+2); //122
+
+// console.log(1+2+"2"); //32
+
+// console.log((3+4)*5%3); //2
+
+
+// console.log(+true); //1
+
+//  console.log(+""); //0
+
+let num1,num2,num3
+
+num1 = num2 = num3 = 2+2
+
+// console.log(num1,num2,num3); //4 4 4
+
+
+let gamecounter = 100
+++gamecounter;
+console.log(gamecounter);
 
 
