4f5a9a08ebde67290b29f78365945395aae0d0a2
shivanithakur07 <23BAI70053@cuchd.in>
2025-09-17 12:48:35 +0000

    commit chnge

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,25 @@
+// console.log(2>1); //true
+// console.log(2>=1); //false
+// console.log(2<1); //false
+// console.log(2 == 1); //false
+// console.log(2 != 1); //true 
+
+
+// console.log("2">1); //true
+// console.log("02">1); //true
+
+console.log(null>0);//false
+console.log(null==0);//false
+console.log(null>=0);//true
+
+
+console.log(undefined==0);//false
+console.log(undefined>0);//false
+console.log(undefined<0);//false
+
+
+// ===(strict equality check operator) and ==(loose equality check operator)
+console.log("2"==2); //true
+console.log("2"===2); //false
+
+console.log(undefined==null);//true
