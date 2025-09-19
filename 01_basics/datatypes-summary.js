9d6b74c67f0bdd5e0480d24a7b652e96431c4707
shivanithakur07 <23BAI70053@cuchd.in>
2025-09-19 12:08:08 +0000

    commit chnge

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -22,7 +22,7 @@ console.log(id===anotherId); //false
 
 
 const bigNumber = 123456789012345678901230n //BigInt
-console.log(bigNumber); 
+// console.log(bigNumber); 
 //non-primitive data type or reference data type
 //1. object
 //2.Array
@@ -39,4 +39,26 @@ const myfunction = function(){
 
     console.log("hello world");
 }
-console.log(typeof anotherId); 
+console.log(typeof id); 
+
+
+
+
+// Memories
+//stack memory - primitive data types are stored in stack memory
+//heap memory - non-primitive data types are stored in heap memory  
+
+let myYoutubename = "shivanithakur"
+let anotherName = "chai or code"
+console.log(myYoutubename);
+console.log(anotherName);
+
+
+let userOne = {
+    email: "user@google.com",
+    upi: "user@axl"
+}
+let userTwo = userOne
+userTwo.email = "shivanithakur@google.com"
+console.log(userOne.email);
+console.log(userTwo.email);
